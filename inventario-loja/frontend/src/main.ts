d3f94f46ec29db0b382486d946187bb0d8ca30d6
kimikdark <tatianamendes.contacto@gmail.com>
2025-07-09 22:59:49 +0100

    Delete inventario-loja/frontend directory

--- inventario-loja/frontend/src/main.ts
+++
@@ -1,9 +0,0 @@
-  // src/main.ts
-import 'zone.js'; // <-- ESTA LINHA É ABSOLUTAMENTE OBRIGATÓRIA E DEVE ESTAR AQUI
-
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config'; // Onde as rotas e providers estão configurados
-import { AppComponent } from './app/app'; // O seu componente raiz standalone
-
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
